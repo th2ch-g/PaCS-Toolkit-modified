db2e157a0f68acf9ef9988faefacb0e85bd775c5
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-12-13 02:49:30 +0000

    add

--- pacs_ta/models/settings.py
+++ pacs_ta/models/settings.py
@@ -205,6 +205,7 @@ class MDsettings:
             "rmsd",
             "ee",
             "a_d",
+            "target_association",
             "template",
         ]:
             LOGGER.error(f"{self.type} is not supported")
